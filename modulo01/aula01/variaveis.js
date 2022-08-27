1316987fd1892fb90cc55660ca24d4a9b2b1abf2
prsonda <prsonda@gmail.com>
2022-08-26 23:15:17 -0300

    Tipos de variáveis

---
+++ modulo01/aula01/variaveis.js
@@ -0,0 +1,14 @@
+var nome = 'Paulo Robson';
+console.log(nome);
+
+var numero = 10;
+console.log(10);
+
+var floatNumber = 3.5;
+console.log(floatNumber);
+
+const bool = true;
+console.log(bool);
+
+let nomes = ['Paulo', 'Ellen', 'Gabriel', 'Samuel'];
+console.log(nomes);
