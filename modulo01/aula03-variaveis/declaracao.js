33519a11eeafd599736adc4d94968439c094f4db
prsonda <prsonda@gmail.com>
2022-08-29 17:56:09 -0300

    Atualização variáveis e exercício

---
+++ modulo01/aula03-variaveis/declaracao.js
@@ -0,0 +1,15 @@
+const nome = 'Paulo Robson',
+  sobrenome = 'da Silva';
+console.log(nome + ' ' + sobrenome);
+
+let numero = 10;
+console.log(10);
+
+let numeroFloat = 3.5;
+console.log(numeroFloat);
+
+let bool = true;
+console.log(bool);
+
+let nomes = ['Paulo', 'Ellen', 'Gabriel', 'Samuel'];
+console.log(nomes);
---
+++ modulo01/aula03-variaveis/exercicio-variaveis.js
@@ -0,0 +1,11 @@
+const cidade = 'Curitiba';
+
+console.log(cidade);
+
+let populacao = 1963726;
+
+console.log(populacao);
+
+populacao = 2000000;
+
+console.log(populacao);
