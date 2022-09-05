c8d045d0d89c53a0ba1557bd0e8b766585512bc4
prsonda <prsonda@gmail.com>
2022-09-05 20:16:34 -0300

    Exercício 06 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/06/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,15 @@
 const alturaEmCm = 185;
 
 //seu código aqui
+
+if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+  console.log('LÍBERO');
+} else if (alturaEmCm > 185 && alturaEmCm <= 195) {
+  console.log('PONTEIRO');
+} else if (alturaEmCm > 195 && alturaEmCm <= 205) {
+  console.log('OPOSTO');
+} else if (alturaEmCm > 205) {
+  console.log('CENTRAL');
+} else {
+  console.log('REPROVADO');
+}
