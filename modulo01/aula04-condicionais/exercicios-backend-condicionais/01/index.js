414c81b43076971acb5b8aee8b9fef9156a9d520
prsonda <prsonda@gmail.com>
2022-09-05 19:01:23 -0300

    Exercício 01 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/01/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/01/index.js
@@ -1,4 +1,27 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = 'pedra';
+const jogada2 = 'tesoura';
 
 //seu código aqui
+/*
+Pedra ganha da tesoura (amassando-a ou quebrando-a).
+Tesoura ganha do papel (cortando-o).
+Papel ganha da pedra (embrulhando-a).
+*/
+
+const jogada1Ganha =
+  (jogada1 === 'pedra' && jogada2 === 'tesoura') ||
+  (jogada1 === 'tesoura' && jogada2 === 'papel') ||
+  (jogada1 === 'papel' && jogada2 === 'pedra');
+
+const jogada2Ganha =
+  (jogada1 === 'tesoura' && jogada2 === 'pedra') ||
+  (jogada1 === 'papel' && jogada2 === 'tesoura') ||
+  (jogada1 === 'pedra' && jogada2 === 'papel');
+
+if (jogada1Ganha) {
+  console.log('Jogador1 venceu!');
+} else if (jogada2Ganha) {
+  console.log('Jogador2 venceu!');
+} else {
+  console.log('Empate');
+}
