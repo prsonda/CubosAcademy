7c6ac0c3217711999a4093f89d1d16cb59a2cc68
prsonda <prsonda@gmail.com>
2022-09-05 19:19:07 -0300

    Exercício 02 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/02/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/02/index.js
@@ -2,3 +2,14 @@ const jogada1 = 5;
 const jogada2 = 3;
 
 //seu código aqui
+
+const resultado = jogada1 + jogada2;
+const par = resultado % 2 === 0;
+const impar = resultado % 2 !== 0;
+const jogador1 = par;
+
+if (jogador1) {
+  console.log('Jogador1 é o vencedor!');
+} else {
+  console.log('Jogador2 é o vencedor!');
+}
