4871adc6678e93a7c0facb185ffe92ea764ba230
prsonda <prsonda@gmail.com>
2022-09-05 20:30:52 -0300

    Exercício 07 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/07/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/07/index.js
@@ -3,3 +3,13 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+
+const corte = 28559.7;
+
+if (aposentada || portadoraDeDoenca) {
+  console.log('ISENTA');
+} else if (totalDeRendimentos <= corte) {
+  console.log('VAZA LEAO! JA TA DIFICIL SEM VOCE');
+} else {
+  console.log('PEGA LEAO');
+}
