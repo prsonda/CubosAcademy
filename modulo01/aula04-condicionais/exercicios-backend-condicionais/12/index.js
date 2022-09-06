87325c6507dd637805262417b031777f7c4941fb
prsonda <prsonda@gmail.com>
2022-09-06 00:13:34 -0300

    Segunda parte exercício 12 concluída

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/12/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/12/index.js
@@ -1,4 +1,4 @@
-const diaDaSemana = 3;
+const diaDaSemana = 10;
 
 if (diaDaSemana === 1) {
   console.log('Segunda Feira');
@@ -14,4 +14,6 @@ if (diaDaSemana === 1) {
   console.log('Sábado');
 } else if (diaDaSemana === 7) {
   console.log('Domingo');
+} else {
+  console.log('O dia da semana informado não é válido.');
 }
