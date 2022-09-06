a7eb4a68531603b8c7fa6f19e18e8dae7ae821c4
prsonda <prsonda@gmail.com>
2022-09-05 21:01:05 -0300

    Exercício 08 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/08/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/08/index.js
@@ -3,3 +3,10 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if (possuiPatologia || idade < 12 || idade > 65 || altura < 150) {
+  console.log('ACESSO NEGADO');
+} else if (idade < 18 || ehEstudante) {
+  console.log('10 reais');
+} else {
+  console.log('20 reais');
+}
