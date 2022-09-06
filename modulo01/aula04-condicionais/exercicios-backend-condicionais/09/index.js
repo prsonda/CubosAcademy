27886851630d68fc0f0b78f3335a5deedadb60c6
prsonda <prsonda@gmail.com>
2022-09-05 21:13:29 -0300

    Exercício 09 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/09/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,18 @@
 const nota = 8.5;
+
+if (nota < 4) {
+  let notaConvertida = 'E';
+  console.log(`O estudando obteve conceito ${notaConvertida}`);
+} else if (nota >= 4 && nota < 6) {
+  let notaConvertida = 'D';
+  console.log(`O estudando obteve conceito ${notaConvertida}`);
+} else if (nota >= 6 && nota < 8) {
+  let notaConvertida = 'C';
+  console.log(`O estudando obteve conceito ${notaConvertida}`);
+} else if (nota >= 8 && nota < 9) {
+  let notaConvertida = 'B';
+  console.log(`O estudando obteve conceito ${notaConvertida}`);
+} else if (nota >= 9 && nota < 10) {
+  let notaConvertida = 'A';
+  console.log(`O estudando obteve conceito ${notaConvertida}`);
+}
