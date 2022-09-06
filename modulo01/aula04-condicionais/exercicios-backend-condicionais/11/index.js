019e3543923a5eb59074c393b6928b5bd22b5b4a
prsonda <prsonda@gmail.com>
2022-09-05 22:29:52 -0300

    Primeira parte exercício 11 concluída

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/11/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/11/index.js
@@ -6,3 +6,16 @@ const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+if (rendaMensalEmCentavos > 200000) {
+  if (totalJaPagoPeloAluno < 1800000) {
+    let valorParcela = ((rendaMensalEmCentavos * 0.18) / 100).toFixed(2);
+    console.log(`O valor da parcela desse mês é R$ ${valorParcela} reais`);
+  }
+} else if (rendaMensalEmCentavos <= 200000) {
+  console.log(
+    'O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.'
+  );
+} else if (totalJaPagoPeloAluno >= 1800000 || mesesDecorridos > 60) {
+  console.log('Não deve nada!');
+}
