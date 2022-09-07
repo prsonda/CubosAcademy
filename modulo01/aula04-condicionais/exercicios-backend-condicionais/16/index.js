9db103934c2d8ad29dbbb703acb33e500ea0971f
prsonda <prsonda@gmail.com>
2022-09-06 22:33:49 -0300

    teste 2

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/16/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/16/index.js
@@ -1,5 +1,5 @@
 //Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
+const idadeDoAluno = 18;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
