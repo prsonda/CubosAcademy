6b0016bf1139297a55b050d77b466d4fbdabab1d
prsonda <prsonda@gmail.com>
2022-09-06 22:52:12 -0300

    Exercício 17 concluído

--- modulo01/aula04-condicionais/exercicios-backend-condicionais/17/index.js
+++ modulo01/aula04-condicionais/exercicios-backend-condicionais/17/index.js
@@ -6,3 +6,10 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+if (valorPago < valorDoProduto / 100) {
+  const faltaPagar = valorDoProduto / 100 - valorPago;
+  const valorParcela = valorDoProduto / quantidadeDoParcelamento / 100;
+  const parcelasRestantes = faltaPagar / valorParcela;
+  console.log(`Restam ${parcelasRestantes} parcelas de R$${valorParcela}`);
+}
