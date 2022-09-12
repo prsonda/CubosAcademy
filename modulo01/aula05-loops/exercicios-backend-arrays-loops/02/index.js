215f02b142de4b8d9e2b99848b7830eb99e6f46e
prsonda <prsonda@gmail.com>
2022-09-12 18:25:24 -0300

    feito

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/02/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/02/index.js
@@ -1,1 +1,14 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ['A', 'a', 'B', 'C', 'E', 'e'];
+
+let soma = 0;
+for (i = 0; i < letras.length; i++) {
+  if (letras[i] == 'E' || letras[i] == 'e') {
+    soma += 1;
+  }
+}
+
+if (soma > 0) {
+  console.log(`Foram encontradas ${soma} letras "E" ou "e".`);
+} else {
+  console.log('Nenhuma letra "E" ou "e" foi encontrada.');
+}
