a6d76cdc9de1b0d1d40c0d411ada749f169b1ba7
prsonda <prsonda@gmail.com>
2022-09-12 18:54:10 -0300

    feito

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/07/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/07/index.js
@@ -1,1 +1,9 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomes = ['Ana', 'Joana', 'Carlos', 'amanda'];
+
+let nomeComA = [];
+for (nome of nomes) {
+  if (nome.indexOf('A') == 0 || nome.indexOf('a') == 0) {
+    nomeComA.push(nome);
+  }
+}
+console.log(nomeComA);
