6cbccac5709b6bb8c35fbf4f45ca519561731297
prsonda <prsonda@gmail.com>
2022-09-12 19:39:59 -0300

    feito

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/11/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/11/index.js
@@ -1,2 +1,9 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+let novo = [];
+for (numero of original) {
+  if ((numero >= 10 && numero <= 20) || numero > 100) {
+    novo.push(numero);
+  }
+}
+console.log(novo);
