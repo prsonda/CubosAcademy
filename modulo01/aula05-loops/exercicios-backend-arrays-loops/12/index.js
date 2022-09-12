c45bc54367cfb7685f8d62bcdb0f6a9c01689400
prsonda <prsonda@gmail.com>
2022-09-12 19:56:28 -0300

    feito

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/12/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,11 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+const filaDeDentro = ['Jose', 'Maria', 'Joao'];
+const filaDeFora = ['Joana', 'Roberta', 'Marcos', 'Felipe'];
 
+for (fora of filaDeFora) {
+  if (filaDeDentro.length < 5) {
+    filaDeDentro.push(filaDeFora.shift());
+  }
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
