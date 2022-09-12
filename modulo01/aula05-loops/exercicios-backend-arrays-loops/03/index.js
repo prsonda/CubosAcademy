06f9aba81907034ba8cfe5326ed91fdc4e1a480e
prsonda <prsonda@gmail.com>
2022-09-12 18:33:51 -0300

    feito

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/03/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/03/index.js
@@ -1,2 +1,25 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+let soma = 0;
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] === 10) {
+    console.log(i);
+    soma += 1;
+  }
+}
+if (soma == 0) {
+  console.log('-1');
+}
+
+soma = 0;
+const numero = [54, 22, 14, 87, 284];
+
+for (let i = 0; i < numero.length; i++) {
+  if (numero[i] === 10) {
+    console.log(i);
+    soma += 1;
+  }
+}
+if (soma == 0) {
+  console.log('-1');
+}
