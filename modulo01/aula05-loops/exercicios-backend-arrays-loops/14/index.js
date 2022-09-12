be4f0a1fce4095f2bd20a5dd7f3e21731acf6d98
prsonda <prsonda@gmail.com>
2022-09-12 20:34:27 -0300

    feito

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/14/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/14/index.js
@@ -1,3 +1,14 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+for (let a = 0; a < arrayA.length; a++) {
+  let segundo = 0;
+  for (b of arrayB) {
+    segundo = arrayB[a];
+  }
+  if (arrayA[a] < segundo) {
+    console.log(arrayA[a]);
+  } else {
+    console.log(arrayB[a]);
+  }
+}
