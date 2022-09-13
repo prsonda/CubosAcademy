5b4d34b63c43e7adede17dfddec7f71bd3073a91
prsonda <prsonda@gmail.com>
2022-09-12 21:44:48 -0300

    melhoria no código com função de callback

--- modulo01/aula05-loops/exercicios-backend-arrays-loops/10/index.js
+++ modulo01/aula05-loops/exercicios-backend-arrays-loops/10/index.js
@@ -1,12 +1,14 @@
 const numeros = [8, 11, 4, 1];
 
 let diferenca = 0;
-let maior = 0;
-let menor = numeros.sort()[0];
-for (n of numeros) {
-  if (n > maior) {
-    maior = n;
-  }
-}
+
+// função de callback para ordenar o array
+numeros.sort(function (a, b) {
+  return a - b;
+});
+
+let maior = numeros[numeros.length - 1];
+let menor = numeros[0];
+
 diferenca = maior - menor;
 console.log(diferenca);
