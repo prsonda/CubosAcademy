eecfc2388ea30be7882e3b822c688aa955ed88e9
prsonda <prsonda@gmail.com>
2022-09-21 16:48:31 -0300

    feito

---
+++ modulo01/aula07-exercicios-backend-objetos/01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+  rua: 'Rua das Margaridas',
+  numero: 305,
+  complemento: 'Casa 1',
+  bairro: 'Sitio Cercado',
+  cep: 81920575,
+  cidade: 'Curitiba',
+  estado: 'PR',
+  pais: 'Brasil',
+};
+
+const usuario = {
+  nome: 'Paulo Robson',
+  email: 'email@email.com',
+  telefone: 41988888888,
+  data_nascimento: '12/06/1984',
+  endereco,
+};
+
+console.log(usuario);
