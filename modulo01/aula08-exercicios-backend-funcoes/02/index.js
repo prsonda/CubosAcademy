4c4ba31af0162b219a1876a864f339e914b0a0c3
prsonda <prsonda@gmail.com>
2022-09-26 18:01:36 -0300

    refatorado

--- modulo01/aula08-exercicios-backend-funcoes/02/index.js
+++ modulo01/aula08-exercicios-backend-funcoes/02/index.js
@@ -1,19 +1,31 @@
 const carro = {
   ligado: false,
   velocidade: 0,
+  mesagemLigadoDesligado: function () {
+    if (this.ligado) {
+      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
+    } else {
+      console.log(`Carro desligado. Velocidade: ${this.velocidade}.`);
+    }
+  },
   ligar: function () {
     if (this.ligado) {
       console.log('Este carro já está ligado.');
     } else {
       this.ligado = true;
-      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
+      this.mesagemLigadoDesligado();
     }
   },
   desligar: function () {
     if (this.ligado) {
-      this.ligado = false;
-      this.velocidade = 0;
-      console.log(`Carro desligado. Velocidade: ${this.velocidade}.`);
+      if (this.velocidade === 0) {
+        this.ligado = false;
+        this.mesagemLigadoDesligado();
+      } else {
+        console.log(
+          `Não é possível desligar o carro em movimento. Velocidade: ${this.velocidade}.`
+        );
+      }
     } else {
       console.log('Este carro já está desligado.');
     }
@@ -21,15 +33,21 @@ const carro = {
   acelerar: function () {
     if (this.ligado) {
       this.velocidade += 10;
-      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
+      this.mesagemLigadoDesligado();
     } else {
       console.log('Não é possível acelerar um carro desligado.');
     }
   },
   desacelerar: function () {
     if (this.ligado) {
-      this.velocidade += -10;
-      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
+      if (this.velocidade >= 10) {
+        this.velocidade += -10;
+        this.mesagemLigadoDesligado();
+      } else {
+        console.log(
+          `Não é possível desacelerar um carro parado: ${this.velocidade}.`
+        );
+      }
     } else {
       console.log('Não é possível desacelerar um carro desligado.');
     }
@@ -45,3 +63,5 @@ carro.desacelerar();
 carro.desligar();
 carro.acelerar();
 carro.desacelerar();
+carro.desacelerar();
+carro.desligar();
