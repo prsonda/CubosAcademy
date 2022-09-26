4ed66b73f3f750b51018550afee2f50ea3ecb1dd
prsonda <prsonda@gmail.com>
2022-09-26 17:19:49 -0300

    item c feito

--- modulo01/aula08-exercicios-backend-funcoes/02/index.js
+++ modulo01/aula08-exercicios-backend-funcoes/02/index.js
@@ -8,6 +8,15 @@ const carro = {
       this.ligado = true;
     }
   },
+  desligar: function () {
+    if (this.ligado) {
+      this.ligado = false;
+      this.velocidade = 0;
+    } else {
+      console.log('Este carro já está desligado.');
+    }
+  },
 };
 
 carro.ligar();
+carro.desligar();
