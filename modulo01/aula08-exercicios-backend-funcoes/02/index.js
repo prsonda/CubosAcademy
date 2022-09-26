db3bd19296e87d5f447ab85ff67741fac4d458b9
prsonda <prsonda@gmail.com>
2022-09-26 17:31:48 -0300

    item f feito

--- modulo01/aula08-exercicios-backend-funcoes/02/index.js
+++ modulo01/aula08-exercicios-backend-funcoes/02/index.js
@@ -6,26 +6,30 @@ const carro = {
       console.log('Este carro já está ligado.');
     } else {
       this.ligado = true;
+      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
     }
   },
   desligar: function () {
     if (this.ligado) {
       this.ligado = false;
       this.velocidade = 0;
+      console.log(`Carro desligado. Velocidade: ${this.velocidade}.`);
     } else {
       console.log('Este carro já está desligado.');
     }
   },
   acelerar: function () {
     if (this.ligado) {
-      this.velocidade = 10;
+      this.velocidade += 10;
+      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
     } else {
       console.log('Não é possível acelerar um carro desligado.');
     }
   },
   desacelerar: function () {
     if (this.ligado) {
-      this.velocidade = -10;
+      this.velocidade += -10;
+      console.log(`Carro ligado. Velocidade: ${this.velocidade}.`);
     } else {
       console.log('Não é possível desacelerar um carro desligado.');
     }
