eb998117d1f53995b90bfa32f2a37f4d55c8423f
prsonda <prsonda@gmail.com>
2022-09-26 22:16:12 -0300

    refatorado

--- modulo01/aula08-exercicios-backend-funcoes/03/index.js
+++ modulo01/aula08-exercicios-backend-funcoes/03/index.js
@@ -17,27 +17,56 @@ const carrinho = {
   calcula: function () {
     let somaItens = 0;
     let valorTotal = 0;
+
     for (item of carrinho.produtos) {
       if (item.precoUnit) {
         somaItens += item.qtd;
         valorTotal += (item.precoUnit * item.qtd) / 100;
       }
     }
+
     const resultado = [valorTotal, somaItens];
     return resultado;
   },
   calcularDesconto: function () {
+    let maisBarato = 999999999999999;
+    let desconto = 0;
+
+    for (item of carrinho.produtos) {
+      if (item.precoUnit < maisBarato) {
+        maisBarato = item.precoUnit;
+      }
+    }
+
+    if (this.calcula()[0] > 10000 || this.calcula()[1] > 4) {
+      const desconto1 = this.calcula()[0] * 0.1;
+      const desconto2 = maisBarato;
+
+      if (desconto1 > desconto2) {
+        desconto = desconto1 / 100;
+      } else {
+        desconto = desconto2 / 100;
+      }
+    }
+
     return desconto;
+  },
+  totalAPagar: function () {
+    const valorPagar = (this.calcula()[0] - this.calcularDesconto()).toFixed(2);
+    return valorPagar;
   },
   addProduto: function (produto) {
     let encontrado = false;
+
     for (prod of this.produtos) {
       let codigo = prod.id;
+
       if (produto.id === codigo) {
         encontrado = true;
         prod.qtd += produto.qtd;
       }
     }
+
     if (!encontrado) {
       carrinho.produtos.push(produto);
     }
@@ -45,6 +74,7 @@ const carrinho = {
   imprimirDetalhes: function () {
     console.log(`Cliente: ${carrinho.nomeDoCliente}`);
     console.log();
+
     for (item of this.produtos) {
       console.log(
         `Item ${item.id} - ${item.nome} - ${item.qtd} und - R$ ${(
@@ -52,14 +82,17 @@ const carrinho = {
         ).toFixed(2)}`
       );
     }
+
     console.log();
     console.log(`Total de itens: ${this.calcula()[1]} itens`);
-    console.log(`Total a pagar: R$ ${this.calcula()[0].toFixed(2)}`);
+    console.log(`Total de desconto: R$ ${this.calcularDesconto().toFixed(2)}`);
+    console.log(`Total a pagar: R$ ${this.totalAPagar()}`);
   },
   imprimirResumo: function () {
     console.log(`Cliente: ${carrinho.nomeDoCliente}`);
     console.log(`Total de itens: ${this.calcula()[1]} itens`);
-    console.log(`Total a pagar: R$ ${this.calcula()[0].toFixed(2)}`);
+    console.log(`Total de desconto: R$ ${this.calcularDesconto().toFixed(2)}`);
+    console.log(`Total a pagar: R$ ${this.totalAPagar}`);
   },
 };
 
