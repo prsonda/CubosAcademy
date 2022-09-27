d31d6f33abb983e5a3ad1d2a007eca219f152a18
prsonda <prsonda@gmail.com>
2022-09-26 22:30:43 -0300

    item c feito

--- modulo01/aula08-exercicios-backend-funcoes/04/index.js
+++ modulo01/aula08-exercicios-backend-funcoes/04/index.js
@@ -2,5 +2,16 @@ const contaBancaria = {
   nome: 'Maria',
   saldo: 0,
   historicos: [],
-  depositar: function (valor) {},
+  depositar: function (valor) {
+    this.saldo += valor;
+    this.historicos.push({
+      tipo: 'Depósito',
+      valor: valor,
+    });
+    console.log(
+      `Deposito de R$${valor / 100} realizado para o cliente: ${this.nome}`
+    );
+  },
 };
+
+contaBancaria.depositar(10000);
