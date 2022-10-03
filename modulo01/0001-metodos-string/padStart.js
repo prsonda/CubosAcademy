45227a1326f9e2d822e7df8cb3dc9e721d174111
prsonda <prsonda@gmail.com>
2022-10-03 20:12:07 -0300

    padStart formatação

--- modulo01/0001-metodos-string/padStart.js
+++ modulo01/0001-metodos-string/padStart.js
@@ -1,10 +1,19 @@
-let contador = 0;
+// let contador = 0;
 
-let num = 0;
+// let num = 0;
 
-while (contador < 100) {
-  num += 1;
-  alterado = String(num).padStart(3, '000');
-  console.log(alterado);
-  contador += 1;
+// while (contador < 100) {
+//   num += 1;
+//   alterado = String(num).padStart(3, '000');
+//   console.log(alterado);
+//   contador += 1;
+// }
+
+function imprimirData(dia, mes, ano) {
+  let diaFormatado = String(dia).padStart(2, '0');
+  let mesFormatado = String(mes).padStart(2, '0');
+
+  console.log(`${diaFormatado}/${mesFormatado}/${ano}`);
 }
+
+imprimirData(1, 1, 2022);
