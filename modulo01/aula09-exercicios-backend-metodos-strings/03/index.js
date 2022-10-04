44c50cf95981920649cc5dd394e91c15d9d3b616
prsonda <prsonda@gmail.com>
2022-10-03 22:20:23 -0300

    feito

--- modulo01/aula09-exercicios-backend-metodos-strings/03/index.js
+++ modulo01/aula09-exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,15 @@
-const texto = "Aprenda programar do zero na Cubos Academy";
+const texto = 'Aprenda programar do zero na Cubos Academy';
+
+let arrayTexto = texto.toLowerCase().split(' ');
+let urlAmigavel = '';
+
+for (let index = 0; index < arrayTexto.length; index++) {
+  const element = arrayTexto[index];
+  if (index === arrayTexto.length - 1) {
+    urlAmigavel += element;
+  } else {
+    urlAmigavel += element + '-';
+  }
+}
+
+console.log(urlAmigavel);
