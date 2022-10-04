64fb43f962e716365c418b051367464a0e1dbc78
prsonda <prsonda@gmail.com>
2022-10-04 00:15:55 -0300

    feito

--- modulo01/aula09-exercicios-backend-metodos-strings/10/index.js
+++ modulo01/aula09-exercicios-backend-metodos-strings/10/index.js
@@ -1,1 +1,23 @@
 const nomeArquivo = 'Foto da Familia.pdf';
+
+// São permitidos arquivos: jpg, jpeg, gif e png
+function verificaArquivo(arquivo) {
+  const posicaoPonto = arquivo.lastIndexOf('.');
+
+  let extencao = '';
+  if (posicaoPonto > -1) {
+    extencao = nomeArquivo.slice(posicaoPonto + 1);
+  }
+  if (
+    extencao === 'jpg' ||
+    extencao === 'jpeg' ||
+    extencao === 'gif' ||
+    extencao === 'png'
+  ) {
+    console.log('Arquivo válido');
+  } else {
+    console.log('Arquivo inválido');
+  }
+}
+
+verificaArquivo(nomeArquivo);
