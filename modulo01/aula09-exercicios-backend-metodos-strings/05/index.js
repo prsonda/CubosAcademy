ffda854ed934e7f80915df2f9a8b2e8cda9d012d
prsonda <prsonda@gmail.com>
2022-10-03 22:49:13 -0300

    feito

--- modulo01/aula09-exercicios-backend-metodos-strings/05/index.js
+++ modulo01/aula09-exercicios-backend-metodos-strings/05/index.js
@@ -1,1 +1,14 @@
 const numeroCartao = '1111222233334444';
+
+const tamenhoCartão = numeroCartao.length;
+
+let numeroCartaoEditado = '';
+
+if (tamenhoCartão === 16) {
+  let primeiraParte = numeroCartao.slice(0, 4);
+  let segundaParte = numeroCartao.slice(12);
+  numeroCartaoEditado += primeiraParte + segundaParte.padStart(12, '*');
+  console.log(numeroCartaoEditado);
+} else {
+  console.log('Cartão inválido');
+}
