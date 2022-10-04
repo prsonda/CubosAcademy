aa7c58149adac8eef47acc0cc4b58cdd3327c0d1
prsonda <prsonda@gmail.com>
2022-10-03 23:50:15 -0300

    feito

--- modulo01/aula09-exercicios-backend-metodos-strings/08/index.js
+++ modulo01/aula09-exercicios-backend-metodos-strings/08/index.js
@@ -1,1 +1,13 @@
-const cpf = "011.022.033-44";
+const cpf = '011.022.033-44';
+
+let cpfAlterado = cpf;
+for (let item of cpf) {
+  if (item === '.' || item === '-' || item === '/') {
+    cpfAlterado = cpfAlterado
+      .replace('.', '')
+      .replace('-', '')
+      .replace('/', '');
+  }
+}
+
+console.log(cpfAlterado);
