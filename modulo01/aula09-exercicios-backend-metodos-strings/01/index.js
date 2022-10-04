a01b2ba4271fd58513fd88672ea03dab2a6dca18
prsonda <prsonda@gmail.com>
2022-10-03 21:44:53 -0300

    feito

--- modulo01/aula09-exercicios-backend-metodos-strings/01/index.js
+++ modulo01/aula09-exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,15 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = 'Esse COVID é muito perigoso!';
+
+let autorizado = true;
+
+for (let item of comentario.toLowerCase().split(' ')) {
+  if (item === 'pandemia' || item === 'covid') {
+    autorizado = false;
+  }
+}
+
+if (autorizado) {
+  console.log('Comentário autorizado');
+} else {
+  console.log('Comentário bloqueado por conter palavras proibidas');
+}
