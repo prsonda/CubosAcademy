80090c6fb38fe66bb769e0c7cad0b30f99a5bfee
prsonda <prsonda@gmail.com>
2022-10-08 22:03:29 -0300

    feito

--- modulo01/aula10-exercicios-backend-metodos-arrays/05/scripts.js
+++ modulo01/aula10-exercicios-backend-metodos-arrays/05/scripts.js
@@ -1,1 +1,32 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+let fila = [];
+
+function agendarPaciente(paciente) {
+  for (let i of paciente) {
+    fila.push(i);
+  }
+}
+
+function atenderPaciente(paciente) {
+  for (let i of paciente) {
+    paciente.pop(i);
+  }
+}
+
+function cancelarAtendimento(paciente) {
+  for (let i of paciente) {
+    if (i == 'Joana') {
+      paciente.pop(i);
+    }
+  }
+}
+
+agendarPaciente(pacientes);
+console.log(fila);
+
+cancelarAtendimento(fila);
+console.log(fila);
+
+atenderPaciente(fila);
+console.log(fila);
