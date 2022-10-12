f35a37f6aae33827f490ebf66ffdcfc57f9085b5
prsonda <prsonda@gmail.com>
2022-10-12 01:28:01 -0300

    feito

--- modulo01/aula11-desafio-do-modulo-01/0007-Contador-de-palavras.js
+++ modulo01/aula11-desafio-do-modulo-01/0007-Contador-de-palavras.js
@@ -1,8 +1,21 @@
 const texto =
-  'Cuidado, pois usuarios as vezes deixam espacos vazios no fim do texto sem querer';
+  'Cuidado,  /  . ? pois usuarios as vezes deixam espacos vazios no fim do texto sem querer';
 
 const lista = texto.trim().split(' ');
 
-console.log(lista.length);
-
-// já tentei com a replece para tirar caracteres especiais e mesmo assim apresenta erro
+let soma = 0;
+for (const palavra of lista) {
+  if (
+    palavra === '' ||
+    palavra === '.' ||
+    palavra === ',' ||
+    palavra === '!' ||
+    palavra === '?' ||
+    palavra === '/' ||
+    palavra === /[^\w\s]/gi
+  ) {
+  } else {
+    soma += 1;
+  }
+}
+console.log(soma);
