2e650523768b160485347da147babc33b535af1f
prsonda <prsonda@gmail.com>
2022-10-24 21:59:49 -0300

    feito

--- modulo02/0001-exercicios-backend-callback/04/index.js
+++ modulo02/0001-exercicios-backend-callback/04/index.js
@@ -1,6 +1,25 @@
-
-
 let tempoParaAlarmarEmSegundos = 10;
 let tempoAlarmandoEmSegundos = 5;
 
+const temporizador = tempoBipando => {
+  console.log(
+    `Timer iniciado, disparando alarme em ${tempoParaAlarmarEmSegundos} segundos!`
+  );
 
+  const tempoEspera = () => {
+    let contador = 1;
+
+    const contagem = () => {
+      console.log('Beep beep!');
+      contador++;
+      if (contador > tempoBipando) {
+        clearInterval(intervalo);
+      }
+    };
+    const intervalo = setInterval(contagem, 1000);
+  };
+
+  setTimeout(tempoEspera, tempoParaAlarmarEmSegundos * 1000);
+};
+
+temporizador(tempoAlarmandoEmSegundos);
