9d1d15d463fd109cf6903fde55e7d1aef89b5d96
prsonda <prsonda@gmail.com>
2022-10-27 22:59:20 -0300

    Código melhorado

--- modulo02/0001-exercicios-backend-callback/05/index.js
+++ modulo02/0001-exercicios-backend-callback/05/index.js
@@ -3,13 +3,10 @@ let tempoAlarmandoEmSegundos = 5;
 let tempoSonecaEmSegundos = 10;
 let numeroDeSonecas = 2;
 
-let tempoAlarme = tempoParaAlarmarEmSegundos * 1000;
-let tempoAlarmando = tempoAlarmandoEmSegundos * 1000;
-let tempoSoneca = tempoSonecaEmSegundos * 1000;
+const tempo =
+  tempoParaAlarmarEmSegundos * 1000 + tempoAlarmandoEmSegundos * 1000 + 100;
 
-let tempoTotal = tempoAlarmando + tempoAlarme + tempoSoneca;
-
-const temporizador = tempoBipando => {
+function alarme(tempoBipando, callback) {
   console.log(
     `Timer iniciado, disparando alarme em ${tempoParaAlarmarEmSegundos} segundos!`
   );
@@ -28,43 +25,34 @@ const temporizador = tempoBipando => {
   };
 
   setTimeout(tempoEspera, tempoParaAlarmarEmSegundos * 1000);
-};
 
-temporizador(tempoAlarmandoEmSegundos);
-
-// segunda parte
-
-let soneca = 1;
-
-while (soneca < numeroDeSonecas + 1) {
-  const mensagemsoneca = () => {
-    console.log(
-      `Soneca ativada, próximo alarme em ${tempoSonecaEmSegundos} segundos`
-    );
-  };
-
-  const tempoMensagem1 = tempoAlarmando + tempoAlarme;
-
-  setTimeout(mensagemsoneca, tempoMensagem1 * soneca + 10);
-
-  const sonecaEspera = tempoBipando => {
-    const tempoEspera = () => {
-      let contador = 1;
-
-      const contagem = () => {
-        console.log('Beep beep!');
-        contador++;
-        if (contador > tempoBipando) {
-          clearInterval(intervalo);
-        }
-      };
-      const intervalo = setInterval(contagem, 1000);
-    };
-    const espera = tempoTotal * soneca;
-    setTimeout(tempoEspera, espera);
-  };
-
-  sonecaEspera(tempoAlarmandoEmSegundos);
-
-  soneca++;
+  callback();
 }
+
+function soneca() {
+  for (let index = 1; index <= numeroDeSonecas; index++) {
+    function e() {
+      console.log(
+        `Soneca ativada, próximo alarme em ${tempoSonecaEmSegundos} segundos`
+      );
+
+      const tempoEspera = () => {
+        let contador = 1;
+
+        const contagem = () => {
+          console.log('Beep beep!');
+          contador++;
+          if (contador > tempoAlarmandoEmSegundos) {
+            clearInterval(intervalo);
+          }
+        };
+        const intervalo = setInterval(contagem, 1000);
+      };
+
+      setTimeout(tempoEspera, tempoSonecaEmSegundos * 1000);
+    }
+    setTimeout(e, index * tempo);
+  }
+}
+
+alarme(tempoAlarmandoEmSegundos, soneca);
