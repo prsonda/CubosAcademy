c2766fdd1e89b3af4910c495e5faa8f8b15047cf
prsonda <prsonda@gmail.com>
2023-01-13 17:37:59 -0300

    feito até o número 5

--- modulo03/001-exercicios-backend-consultas-sql/01/query.sql
+++ modulo03/001-exercicios-backend-consultas-sql/01/query.sql
@@ -1,1 +1,11 @@
+select * from musicas;
 
+select * from musicas;
+
+select composicao, tempo from musicas where tempo > 60*4;
+
+select composicao, tempo from musicas where tempo > 60*4;
+
+select * from musicas where compositor is not null and tempo < 60*5 and compositor != 'Bach';
+
+select composicao, tempo from musicas where compositor = 'Mozart' or compositor = 'Bach';
