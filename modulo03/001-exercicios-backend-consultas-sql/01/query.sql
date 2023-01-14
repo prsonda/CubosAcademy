c9025c89f45c536f5d358633332c11d1fadf28e9
prsonda <prsonda@gmail.com>
2023-01-14 01:22:56 -0300

    feito aé o 10

--- modulo03/001-exercicios-backend-consultas-sql/01/query.sql
+++ modulo03/001-exercicios-backend-consultas-sql/01/query.sql
@@ -1,7 +1,5 @@
 select * from musicas;
 
-select * from musicas;
-
 select composicao, tempo from musicas where tempo > 60*4;
 
 select composicao, tempo from musicas where tempo > 60*4;
@@ -9,3 +7,13 @@ select composicao, tempo from musicas where tempo > 60*4;
 select * from musicas where compositor is not null and tempo < 60*5 and compositor != 'Bach';
 
 select composicao, tempo from musicas where compositor = 'Mozart' or compositor = 'Bach';
+
+select * from musicas order by id desc;
+
+select * from musicas order by tempo desc;
+
+select * from musicas order by tempo asc limit 5;
+
+select * from musicas order by tempo desc limit 10;
+
+select * from musicas order by tempo asc offset 5 limit 10;
