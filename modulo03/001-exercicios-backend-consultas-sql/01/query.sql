6fbd3048910737b7607b22ba65fd4741430fde31
prsonda <prsonda@gmail.com>
2023-01-14 22:49:13 -0300

    feito até o 15

--- modulo03/001-exercicios-backend-consultas-sql/01/query.sql
+++ modulo03/001-exercicios-backend-consultas-sql/01/query.sql
@@ -17,3 +17,13 @@ select * from musicas order by tempo asc limit 5;
 select * from musicas order by tempo desc limit 10;
 
 select * from musicas order by tempo asc offset 5 limit 10;
+
+select * from musicas limit 10 offset 10*4;
+
+select * from musicas limit 12 offset 12*6;
+
+select distinct compositor from musicas where compositor is not null;
+
+select distinct compositor, composicao from musicas;
+
+select * from musicas where compositor like 'Bra%';
