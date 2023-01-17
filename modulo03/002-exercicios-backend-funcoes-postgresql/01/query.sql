a81cf58f35a2aad482bdb45c83d8c179b4138943
prsonda <prsonda@gmail.com>
2023-01-17 00:01:24 -0300

    feito até o 5

--- modulo03/002-exercicios-backend-funcoes-postgresql/01/query.sql
+++ modulo03/002-exercicios-backend-funcoes-postgresql/01/query.sql
@@ -1,1 +1,9 @@
+SELECT COUNT(*) FROM farmacia;
 
+select min(idade) from usuarios;
+
+select max(idade) from usuarios;
+
+select avg(idade) from usuarios where idade >= 18
+
+select sum(estoque) from farmacia where categoria = 'blue' or categoria = 'black'
